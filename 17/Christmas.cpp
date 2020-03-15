52f74aed6f9a149f2d0d7f22d7c8ef92f4ffa61e
CHO <ekfwndls@gmail.com>
2020-03-15 15:00:07 +0000

    200315

--- 17/Christmas.cpp
+++ 17/Christmas.cpp
@@ -4,6 +4,35 @@
 #include <cmath>
 using namespace std;
 const int  mod = 20091101;
+// Problem 1.
+// psum 이 같은 것들 끼리 묶은 집합, psum 첫 원소 전 0이 있다고 가정
+int waysToBuy(const vector<int> &psum, int _childNum){
+ 	int ret=0;
+	vector<long long> count(_childNum,0);
+	for(int i=0;i<psum.size();i++){
+		count[psum[i]]++;
+	}
+	for(int i=0;i<_childNum;i++){
+		if(count[i]>=2)
+			ret = (ret+ ((count[i]*(count[i]-1))/2)) % mod;
+	}	
+	return ret;
+}
+int maxBuys(const vector<int> &psum, int _childNum){
+	vector<int> dp(psum.size(),0);
+	// prev[a] = psum[] == a인 위치 
+	// 즉 j~i 까지 연속으로 구매한다고 가정할 때(psum[j-1]==psum[i]) 가장 큰 j 
+	vector<int> prev(_childNum,-1);
+	for(int i=0;i<psum.size();i++){
+		if (i>0) dp[i] = dp[i-1];
+		else dp[i] = 0;
+		int temp = prev[psum[i]];
+		if(temp!= -1) dp[i] = max(dp[i], dp[temp]+1);
+		//prev[i] 새로 저장
+		prev[psum[i]]= i;
+	}
+	return dp.back();
+}
 int main(){
 	ios_base::sync_with_stdio(false); 
 	cin.tie(NULL);
@@ -17,7 +46,6 @@ int main(){
 		// 상자 번호 별 인형 수
 		vector<int> dollNum(boxNum);
 		for(int i=0;i<boxNum;i++) {
-			dollNum[i] = 0;
 			cin>>dollNum[i];
 		}
 		// 인형들 박스의 부분합
@@ -25,36 +53,9 @@ int main(){
 		//psum[-1] == 0을 아래처럼 가정 => 첫 상자부터 사는 경우 고려
 		psum[0] = 0;
 		for(int i=1;i<=boxNum;i++){
-			psum[i] = (psum[i-1]+ dollNum[i])%mod;
-		}
-		// Problem 1.
-		// psum 이 같은 것들 끼리 묶은 집합, psum 첫 원소 전 0이 있다고 가정
-		vector<long long> count(childNum);
-		for(int i=0;i<psum.size();i++){
-			count[psum[i]]++;
-		}
-		int ans_1=0;
-		for(int i=0;i<childNum;i++){
-			if(count[i]>=2)
-				ans_1 += ((count[i]*(count[i]-1))/2)%mod;
-		}
-		cout<<ans_1<<' ';
-		// Problem 2.
-		vector<int> dp(boxNum+1,0);
-		// prev[a] = psum[] == a인 위치 
-		// 즉 j~i 까지 연속으로 구매한다고 가정할 때(psum[j-1]==psum[i]) 가장 큰 j 
-		vector<int> prev(childNum,-1);
-		dp[0] = 0;
-		for(int i=1;i<boxNum+1;i++){
-			dp[i] = dp[i-1];
-			int temp = prev[dp[i]];
-			if(temp!= -1) dp[i] = max(dp[i], dp[temp]+1);
-			//prev[i] 새로 저장
-			prev[i] = i;
-		}
-		cout<< dp[boxNum]<<'\n';	
-		
-		
-	}		
-
+			psum[i] = (psum[i-1]+ dollNum[i-1])%childNum; // psum[0] = 0 이므로 dollNum의 index 가 i가 아니라 i-1
+ 		}
+		cout<<waysToBuy(psum, childNum) << " "<< maxBuys(psum,childNum)<<'\n';
+	}
+	return 0;
 }
