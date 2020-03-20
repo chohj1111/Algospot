d5426c05dbe242406adc8895005c69e6c9ea9b5d
CHO <ekfwndls@gmail.com>
2020-03-20 14:49:45 +0000

    200320

---
+++ 19/Brackets2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <stack>
+using namespace std;
+bool check_stack(const string& str){
+	stack<char> stk;
+	const string opening("({[");
+	const string closing(")}]");
+	for(int i=0;i<str.size();i++){
+		if(opening.find(str[i]) != -1){
+			stk.push(str[i]);
+		}
+		else{
+			if(stk.empty()) 	return false;
+			if(opening.find(stk.top())!=closing.find(str[i]))
+				return false;
+			stk.pop();
+		}
+	}
+	return stk.empty();
+}
+int main(){
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int test;
+	cin>>test;
+	while(test--){
+		string str;
+		cin >> str;
+
+		if(check_stack(str)) cout<<"YES"<<endl;
+		else cout<<"NO"<<endl;
+			
+	}
+	return 0;
+	
+}
