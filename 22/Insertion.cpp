d9928514d1841d4870309d683b8c720b9773c4b6
CHO <ekfwndls@gmail.com>
2020-04-26 14:56:53 +0000

    200426

---
+++ 22/Insertion.cpp
@@ -0,0 +1,33 @@
+// unsolved 
+
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+typedef int KeyType;
+
+int n, shifted[50000];
+int A[50000];
+void solve(){
+	Node* candidates = NULL;
+	
+}
+int main(){
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int test;
+	cin>>test;
+	while(test--){
+		int num;
+		cin>>num;
+		for(int i=0;i<num;i++){
+			cin>>shifted[i];
+		}
+	}
+	
+	return 0;
+}
---
+++ 23/Runningmedian.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+using namespace std;
+vector<int> arr;
+
+
+int main(){
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int test;
+	cin>>test;
+	while(test--){
+		int num, a, b;
+		cin>> num>>a>>b;
+		
+	}
+	
+	return 0;
+}
